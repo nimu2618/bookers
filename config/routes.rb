bf201e55a9ab2192bb6ef19197c83cd7d8c93e28
nimu2618 <119933351+nimu2618@users.noreply.github.com>
2023-01-01 23:44:27 +0000

    1/2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get '/'=>'homes#top'
-  resources :books
+  get 'books/new'
+  get 'books'=>'books#index'
+  post 'books'=>'books#create'
+  get 'books/:id'=>'books#show', as:'book'
+  get 'books/:id/edit'=>'books#edit', as:'edit_book'
+  patch 'books/:id'=>'books#update', as:'update_book'
+  delete 'books/:id'=>'books#destroy', as:'destroy_book'
 end
 
