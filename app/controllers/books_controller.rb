f3b3b5e6f82dd2ccec6be69fe707100f11c51f1e
nimu2618 <119933351+nimu2618@users.noreply.github.com>
2023-01-05 08:27:05 +0000

    フラッシュメッセージupdate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-  protect_from_forgery
   def new
   end
 
@@ -9,8 +8,12 @@ class BooksController < ApplicationController
   end
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+  if  book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)#, notice: "You have created book successfully."
+  else
+    render :index
+  end
   end
   def show
     @book = Book.find(params[:id])
